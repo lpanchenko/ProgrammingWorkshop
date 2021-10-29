ea37fba77856bed7417546355cae02aa4b7ae745
lpanchenko <liuba.panchenko@gmail.com>
2021-10-28 23:09:59 -0400

    Add a second variant of HW2

--- src/main/java/Tasks/HW2.java
+++ src/main/java/Tasks/HW2.java
@@ -1,5 +1,9 @@
 package Tasks;
 
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class HW2 {
@@ -9,21 +13,55 @@ public class HW2 {
         System.out.println("Enter text: ");
         Scanner sc = new Scanner(System.in);
         String text = sc.nextLine();
+
         System.out.println("Enter max length: ");
         int maxLength = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Enter prohibited words: ");
+        List <String> badWorlds = new ArrayList<>();
+        while (sc.hasNextLine())
+        {
+            String line = sc.nextLine();
+            if (line == null || line.isEmpty())
+                break;
+            badWorlds.add(line);
+        }
+        sc.close();
 
         int length = text.length();
-        System.out.println("Text contains " + length + " symbols");
-
         int lengthWithoutSpaces = (int) text.chars().filter(c -> c != ' ').count();
-        System.out.println("Text contains " + lengthWithoutSpaces+ " symbols without spaces");
+        String pureText = getPureText(text, (ArrayList<String>) badWorlds);
+        String pureShortText = getPureText(getShortText(text, maxLength), (ArrayList<String>) badWorlds);
 
-        if (length%2 == 0)
-            System.out.println("Text contains even amount of symbols");
-        else
-            System.out.println("Text contains odd  amount of symbols");
+        JSONObject json = GetJson (text, length, lengthWithoutSpaces, pureText, pureShortText);
+        System.out.println(json);
+    }
 
-        if (length > maxLength)
-            System.out.println("Длина текста превышает длину " + maxLength + " символов");
+    private static JSONObject GetJson(String text, int length, int lengthWithoutSpaces, String pureText, String pureShortText) {
+        JSONObject json = new JSONObject();
+        json.put("length", length);
+        json.put("pure_length", lengthWithoutSpaces);
+        json.put("origin_text", text);
+        json.put("pure_text", pureText);
+        json.put("pure_short_text", pureShortText);
+        return json;
+    }
+
+    private static String getShortText(String text, int maxLength) {
+
+        String shortText = text.substring(0, maxLength + 1);
+        if (text.length() > maxLength)
+            return shortText + "...";
+        return shortText;
+    }
+
+    private static String getPureText(String text, ArrayList<String> worlds) {
+
+        String pureText = new String(text);
+        for (int i = 0; i < worlds.stream().count(); i++) {
+            pureText = pureText.replace(worlds.get(i), "***");
+        }
+        return pureText;
     }
 }
