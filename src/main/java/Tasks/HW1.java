5cc42cb458bf477e042662a8391d4c227f6b1bbf
lpanchenko <liuba.panchenko@gmail.com>
2021-11-01 00:31:45 -0400

    Change HW1 after review

--- src/main/java/Tasks/HW1.java
+++ src/main/java/Tasks/HW1.java
@@ -1,8 +1,5 @@
 package Tasks;
 
-import org.joda.time.DateTime;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -11,23 +8,24 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class HW1 {
 
     public static void main(String[] args) throws IOException {
 
         String userName = System.getProperty("user.name");
-        DateTimeFormatter formatter = DateTimeFormat.forPattern("HH");
-        DateTime dt = new DateTime(GetDataTime());
-        int currentHours = Integer.parseInt(dt.toString(formatter));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH");
+        int currentHours = Integer.parseInt(LocalTime.now().format(formatter));
 
         if (currentHours >= 0 && currentHours <= 4)
             System.out.println("Доброй ночи, " + userName);
-        else if (currentHours > 4 && currentHours <= 9)
+        else if (currentHours <= 9)
             System.out.println("Доброе утро, " + userName);
-        else if (currentHours > 10 && currentHours <= 16)
+        else if ( currentHours <= 16)
             System.out.println("Добрый день, " + userName);
-        else if (currentHours > 16 && currentHours <= 24)
+        else if (currentHours <= 23)
             System.out.println("Добрый вечер, " + userName);
         else System.out.println("Wrong data format " + currentHours);
     }
